5dc6a78e7cfbfd0f7db64835a61d8997e1fa464e
Sadika <najatulislam111@gmail.com>
2026-03-25 22:09:06 +0600

    huh

--- app/[components]/HeroProjects.jsx
+++ app/[components]/HeroProjects.jsx
@@ -49,10 +49,10 @@ const HeroProjects = () => {
         <h1 className='text-[42px] font-semibold text-center pb-10'>Works & Projects</h1>
       {/* FILTER BUTTON */}
       <div className="flex justify-center gap-4 my-10">
-          <button className="filter-btn py-2 px-3 gap-x-2.5 rounded-2xl bg-blue-400 hover:border hover:border-blue-400 hover:bg-transparent" data-filter="all">All</button>
-        <button className="filter-btn py-2 px-3 rounded-2xl my-2.5 gap-x-2.5 bg-blue-400 hover:border hover:border-blue-400 hover:bg-transparent" data-filter=".web">Web Designs</button>
-          <button className="filter-btn py-2 px-3 rounded-2xl gap-x-2.5 bg-blue-400 hover:border hover:border-blue-400 hover:bg-transparent" data-filter=".dev">Web Development</button>
-        <button className="filter-btn py-2 px-3 rounded-2xl gap-x-2.5 my-2 bg-blue-400 hover:border hover:border-blue-400 hover:bg-transparent" data-filter=".dd">Designs with Development</button>
+          <button className="filter-btn py-2 px-3 rounded-2xl bg-blue-400 hover:border hover:border-blue-400 hover:bg-transparent" data-filter="all">All</button>
+        <button className="filter-btn py-2 px-3 rounded-2xl bg-blue-400 hover:border hover:border-blue-400 hover:bg-transparent" data-filter=".web">Web Designs</button>
+          <button className="filter-btn py-2 px-3 rounded-2xl  bg-blue-400 hover:border hover:border-blue-400 hover:bg-transparent" data-filter=".dev">Web Development</button>
+        <button className="filter-btn py-2 px-3 rounded-2xl  bg-blue-400 hover:border hover:border-blue-400 hover:bg-transparent" data-filter=".dd">Designs with Development</button>
         </div>
 
       {/* ITEMS */}
